abd3fcac58e02be393600d9cf406c2ba496673da
Kieran Eglin <kieran.eglin@gmail.com>
2020-08-28 14:34:02 -0700

    Setting up tailwind; Setting up desired TS/ESlint/babel config

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  requirePragma: false,
+  endOfLine: 'lf'
+}
--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <div id="nav" class="btn-blue-border">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
